bb234ece5cfb911c09f88c1b0b74a5dbc866a6d0
Tsche <2440422+Tsche@users.noreply.github.com>
2022-08-27 20:12:31 +0200

    increase delays

--- src/config.py
+++ src/config.py
@@ -53,7 +53,7 @@ from datetime import timedelta
 recheck_time = timedelta(seconds=10)
 
 # after how much time an occupied channel will become dormant
-dormant_time = timedelta(minutes=20)
+dormant_time = timedelta(minutes=40)
 
 # after how much time a dormant channel will be available again
-reset_time = timedelta(minutes=5)
+reset_time = timedelta(minutes=10)
