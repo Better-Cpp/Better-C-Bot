b02c54e67a757c81ae93e26506d25b45bf34d901
makian123 <hohosmarko@gmail.com>
2023-08-20 20:18:17 +0200

    added better validation

--- src/cogs/modmail.py
+++ src/cogs/modmail.py
@@ -2,43 +2,63 @@ import discord
 from discord.ext import commands
 from discord.ui import Button, View, Modal
 from random import randrange
+import time
 from src import config as conf
 from src.util import permissions
 
 class ModmailModal(Modal):
-    answer = discord.ui.TextInput(label = "Answer")
+    name = discord.ui.TextInput(label = "Subject")
+    issue = discord.ui.TextInput(label = "Content", style = discord.TextStyle.paragraph, max_length = 1000)
 
-    def __init__(self):
-        num1 = randrange(10)
-        num2 = randrange(10)
-        super().__init__(title = f"Solve the equation: {num1} + {num2}")
-        self.solution = num1 + num2
+    def __init__(self, selected):
+        super().__init__(title = f"Describe your issue")
+        self.selected = selected
 
     async def on_submit(self, interaction):
-        if(int(self.answer.value) != self.solution):
-            await interaction.response.send_message(f'Wrong answer', ephemeral=True)
-            return
-
         user = interaction.user
         channel: discord.TextChannel = interaction.client.get_channel(conf.modmail_channel)
 
         created_thread = await channel.create_thread(
-            name = f"{user.display_name}'s ticket",
+            name = f"{self.name.value}",
             type = discord.ChannelType.private_thread,
             invitable = False
         )
-        await created_thread.add_user(user)
-        await created_thread.send(f"{user.mention} please describe your issue and wait for <@&{conf.staff_role}> to respond.")
 
-        await interaction.response.send_message(f'Channel has been created!', ephemeral=True)
+        embed = discord.Embed(title = f"**{self.selected}**")
+        embed.add_field(name="", value=f"{self.issue.value}")
+        embed.set_author(name = user.name, icon_url = user.avatar.url)
+
+        await created_thread.add_user(user)
+        await created_thread.send(
+            f"{user.mention} please wait for <@&{conf.staff_role}> to respond.\n",
+            embeds = [embed]
+        )
+
+        await interaction.response.send_message(
+            f"Channel created in <t:{int(time.time())}:f>\n{created_thread.jump_url}", 
+            ephemeral = True
+        )
 
 class ModmailView(View):
     def __init__(self):
         super().__init__(timeout = None)
 
-    @discord.ui.button(label="Create Ticket", custom_id='persistent_view:create_ticket')
+    @discord.ui.select(placeholder = "Select issue type", custom_id = "persistent_view:selection", options = [
+        discord.SelectOption(label = "I want to report a user or message"),
+        discord.SelectOption(label = "There's an issue with a moderative action"),
+        discord.SelectOption(label = "I want to appeal a ban"),
+        discord.SelectOption(label = "Other")
+    ])
+    async def select_type(self, interaction, selection):
+        await interaction.response.defer(ephemeral = True, thinking = False)
+
+    @discord.ui.button(label = "Create ticket", custom_id = "persistent_view:create_ticket")
     async def button_callback(self, interaction, button):
-        modal = await interaction.response.send_modal(ModmailModal())
+        if len(self.children[0].values) == 0:
+            await interaction.response.send_message("Please select an option", ephemeral = True)
+            return
+
+        await interaction.response.send_modal(ModmailModal(self.children[0].values[0]))
 
 class Modmail(commands.Cog):
     def __init__(self, bot):
